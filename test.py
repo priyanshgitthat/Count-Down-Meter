777238fbecfc13ef258250abd3cafb70808819ff
priyansh verma <vermapriyansh68@gmail.com>
2024-08-08 16:04:48 -0700

    done

---
+++ test.py
@@ -0,0 +1,65 @@
+# paise=int(input("Enter Your Amount : "))
+# rs=paise/100
+# print(f"there are {rs} rupees in {paise} paise")
+
+# a=int(input("Enter value of a = "))
+# b=int(input("Enter value of b = "))
+# print(f"value of a is : {a}")
+# print(f"value of b is : {b}")
+# temp = a
+# a = b
+# b = temp
+# print(f"value of a is : {a}")
+# print(f"value of b is : {b}")
+
+# a=int(input("Enter value of a = "))
+# b=int(input("Enter value of b = "))
+# print(f"value of a is : {a}")
+# print(f"value of b is : {b}")
+# a=a+b
+# b=a-b
+# a=a-b
+# print(f"value of a is : {a}")
+# print(f"value of b is : {b}")
+
+# year = int(input("Enter Year = "))
+# if (year % 400 == 0):
+#     print(f"{year} is a leap year !")
+# else:
+#     if (year % 4 ==0 and year % 100 != 0):
+#         print(f"{year} is a leap year ! ")
+#     else:
+#         print(f"{year} is not a leap year !")
+
+# year =int (input ('From How Many Years You Have Been Working = '))
+# sal = float (input ("Enter Your Salary = "))
+# if year >= 10:
+#     bonus= sal * 10 /100
+# elif year >=6 and year <=10:
+#     bonus= sal * 8/100
+# elif year >=1 and year <=6:
+#     bonus= sal * 5/100
+# else:
+#     bonus="no bonus for you"
+# print(bonus)
+
+# i,j,k=3,5,7
+# print(i,j,k)
+# if i<j:
+#     if j<k:
+#         i=j
+#     else:
+#         j=k
+# else:
+#     if j>k:
+#         j=i
+#     else:
+#         i=k
+# print(i,j,k)
+
+import calendar
+print(calendar.month(2029,1))
+
+# from datetime import datetime 
+# n=datetime.now()
+# print(n)
